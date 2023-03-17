555e9d89ca0947f4416dbee5cf26f6cdea3ce3c0
Pedro <127751247+Pedrofmdo@users.noreply.github.com>
2023-03-17 11:29:11 -0300

    Add files via upload

---
+++ teste2.java
@@ -0,0 +1,32 @@
+package esss;
+
+import java.util.Scanner;
+
+public class teste2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.println(" informe um valor: ");
+		int n1 = sc.nextInt();
+		
+		if (n1 % 2 == 0) {
+			System.out.println(n1 + " é par");
+		} else  { 
+			System.out.println(n1 + " é impar");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
