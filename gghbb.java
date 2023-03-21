b9f7b8ba5bbe0d489bad8df6eb16b76ad925b8cb
Pedro <127751247+Pedrofmdo@users.noreply.github.com>
2023-03-21 11:28:57 -0300

    Add files via upload

---
+++ gghbb.java
@@ -0,0 +1,29 @@
+package git;
+
+public class gghbb {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 150;
+		if(a > 150) {
+			if (a <200) {
+				System.out.println("é verdade!!!");
+				
+			}else {
+				System.out.println("não é verdadeiro!!");
+			}
+		}else {
+			System.out.println("não é verdadeiro!!");
+			
+			
+			
+			}
+		}
+		
+		
+		
+		
+	}
+
+
